7de1e98a73bae363384803fb5798fc060f3b0acc
Alberto Gallen <alberto@cooolmod.com>
2022-07-03 20:38:54 +0200

    Create Basic Estructure

---
+++ MagentoApi.ClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagentoApi.ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
